d5178ba70ca82b3988f2a9a9810941843880b3d8
IyzOnline <142793451+IyzOnline@users.noreply.github.com>
2025-05-12 20:10:53 +0800

    docu: Change position of padding block class

--- talyergo/src/components/PrimaryHeader.js
+++ talyergo/src/components/PrimaryHeader.js
@@ -3,16 +3,16 @@ import { Link } from "react-router-dom";
 export const PrimaryHeader = () => {
     return (
         <header>
-            <div className="container">
+            <div className="container | padding-block-32">
                 <div className="primary-header-wrapper">
-                    <p><Link to="/">TalyerGo</Link></p>
-                    <div>
-                        <ul className="ph-nav" role="list"> 
+                    <p className="logo"><Link to="/">TalyerGo</Link></p>
+                    <nav>
+                        <ul className="ph-nav" aria-label="Primary Navigation"> 
                             <li><Link to="/">Home</Link></li>
                             <li><Link to="/">About</Link></li>
                             <li><Link to="/">Shops</Link></li>
                         </ul>
-                    </div>
+                    </nav>
                     <div className="ph-buttons">
                         <button><Link>Sign up</Link></button>
                         <button><Link>Log in</Link></button>
