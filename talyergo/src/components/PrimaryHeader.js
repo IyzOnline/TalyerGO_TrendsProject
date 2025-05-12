ab837c60c5147b5bc067f811041de7d12b8b679d
IyzOnline <142793451+IyzOnline@users.noreply.github.com>
2025-05-12 15:37:08 +0800

    feat: Add Primary Header skeletion

--- talyergo/src/components/PrimaryHeader.js
+++ talyergo/src/components/PrimaryHeader.js
@@ -1,7 +1,24 @@
+import { Link } from "react-router-dom";
+
 export const PrimaryHeader = () => {
     return (
         <header>
-            yes
+            <div className="container">
+                <div className="primary-header-wrapper">
+                    <p><Link to="/">TalyerGo</Link></p>
+                    <div>
+                        <ul className="ph-nav" role="list"> 
+                            <li><Link to="/">Home</Link></li>
+                            <li><Link to="/">About</Link></li>
+                            <li><Link to="/">Shops</Link></li>
+                        </ul>
+                    </div>
+                    <div className="ph-buttons">
+                        <button><Link>Sign up</Link></button>
+                        <button><Link>Log in</Link></button>
+                    </div>
+                </div>
+            </div>
         </header>
     );
 }
