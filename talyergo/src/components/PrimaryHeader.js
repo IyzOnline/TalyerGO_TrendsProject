c2e3a81e8accf261adea9d72134206fd41217f5b
IyzOnline <142793451+IyzOnline@users.noreply.github.com>
2025-05-13 12:56:37 +0800

    feat: Add Shop page link to primary header

--- talyergo/src/components/PrimaryHeader.js
+++ talyergo/src/components/PrimaryHeader.js
@@ -10,7 +10,7 @@ export const PrimaryHeader = () => {
                         <ul className="ph-nav" aria-label="Primary Navigation"> 
                             <li><Link to="/">Home</Link></li>
                             <li><Link to="/">About</Link></li>
-                            <li><Link to="/">Shops</Link></li>
+                            <li><Link to="/shops">Shops</Link></li>
                         </ul>
                     </nav>
                     <div className="ph-buttons">
