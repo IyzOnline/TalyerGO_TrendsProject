80d588fab1fb26502dc8fe8f53c08809d30e8ad7
IyzOnline <142793451+IyzOnline@users.noreply.github.com>
2025-05-13 12:55:38 +0800

    feat: Import Shops page into main App

--- talyergo/src/App.js
+++ talyergo/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Home } from './components/Home.js';
 import { PrimaryHeader } from './components/PrimaryHeader.js';
 import { Footer } from './components/Footer.js';
+import { Shops } from './components/Shops.js';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <PrimaryHeader />
         <Routes>
           <Route path="/" element={<Home />}/>
+          <Route path="/shops" element={<Shops />}/>
         </Routes>
         <Footer />
       </Router>
