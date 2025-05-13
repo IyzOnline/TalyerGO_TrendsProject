fc593f82bf7b42757ac2fbe324ca9aa5f0de294c
IyzOnline <142793451+IyzOnline@users.noreply.github.com>
2025-05-13 12:56:53 +0800

    feat: Add initial Shop page skeleton

---
+++ talyergo/src/components/Shops.js
@@ -0,0 +1,29 @@
+export const Shops = () => {
+   return (
+    <main>
+        <div className="container">
+
+            <div className="shops-wrapper">
+                
+                <section className="shops-heading-wrapper | h1-container container padding-block-80">
+                    <div class="shops-bg-container | bg-container-filter-blue"></div>
+                    <h1 className="main-headings">Local Repair Shops</h1>
+                </section>
+
+                <section className="shops-profile-section">
+                    <input type="text" classNam="shops-search"/>
+                    <div className="shops-profilie-wrapper | padding-block-80">
+                        <div className="shops-profile">
+                            <img src="" alt=""/>
+                            <h2>yes</h2>
+                            <p></p>
+                        </div>
+                    </div>
+                </section>
+                
+            </div>
+
+        </div>
+    </main>
+   ); 
+}
