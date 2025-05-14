feba0ad9c611929f9e7a7e0423025055815ebfa8
IyzOnline <142793451+IyzOnline@users.noreply.github.com>
2025-05-14 13:45:04 +0800

    fix: Changed classes to have prefix LS (login/signup)

--- talyergo/src/components/LogIn.js
+++ talyergo/src/components/LogIn.js
@@ -2,20 +2,20 @@ export const LogIn = () => {
     return (
         <section>
             <div className="container">
-                <div className="log-in-wrapper | padding-block-80">
-                    <div class="log-in-bg-container | bg-container-filter-blue"></div>
-                    <div className="log-in-content-wrapper">
-                        <div className="log-in-logo-wrapper">
+                <div className="LS-wrapper | padding-block-80">
+                    <div class="LS-bg-container | bg-container-filter-blue"></div>
+                    <div className="LS-content-wrapper">
+                        <div className="LS-logo-wrapper">
                             <p className="logo-large">Talyer<span className="clr-accent-300">Go</span></p>
                             <p>Naga City's Mechanic Shop</p>
                             <p>Information Center</p>
                         </div>
-                        <div className="log-in-box-wrapper">
+                        <div className="LS-box-wrapper">
                             <h1>Log In</h1>
                             <input type="text" placeholder="Username / Email"/>
                             <input type="password" placeholder="Password"/>
                             <div className="submit-wrapper">
-                                <button className="log-in-submit">LOG IN</button>
+                                <button className="LS-submit">LOG IN</button>
                                 <div class="bottom-options">
                                     <p>Forgot Password</p>
                                     <p>Sign Up</p>
