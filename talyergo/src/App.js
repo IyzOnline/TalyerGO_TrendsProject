595dbe7f06e0b505ca04f309959ada3134e32c7b
IyzOnline <142793451+IyzOnline@users.noreply.github.com>
2025-05-16 15:14:23 +0800

    feat: Import test provider

--- talyergo/src/App.js
+++ talyergo/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import { ShopsProvider } from './components/ShopsContext.js';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Home } from './components/Home.js';
 import { PrimaryHeader } from './components/PrimaryHeader.js';
